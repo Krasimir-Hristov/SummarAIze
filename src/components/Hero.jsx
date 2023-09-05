58e354e1a0feffcfcd9a232332879b41012c58ad
krasi <krasimir.xristov@gmail.com>
2023-09-05 19:41:50 +0200

    Add my dist folder with the changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,10 @@ const Hero = () => {
         <button
           type="button"
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open(
+              "https://github.com/Krasimir-Hristov/SummarAIze",
+              "_blank"
+            )
           }
           className="black_btn"
         >
