55cdce8c74347ab704d3aef9020c4816e99b0757
krasi <krasimir.xristov@gmail.com>
2023-09-05 19:33:06 +0200

    Update my UI

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,8 @@ const Hero = () => {
       </nav>
 
       <h1 className="head_text">
-        Summarize Articles with <br className="max-md:hidden" />
+        Summar<span className="orange_gradient ">AI</span>ze Articles with{" "}
+        <br className="max-md:hidden" />
         <span className="orange_gradient ">OpenAI GPT-4</span>
       </h1>
       <h2 className="desc">
